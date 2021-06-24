6ce962a195b8e8e3f928eee2dca6e756ddc3013f
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-06-24 09:26:54 +0545

    loop and break

--- Program Files/Program.cs
+++ Program Files/Program.cs
@@ -2,6 +2,7 @@
  * You can try this at home.
  */
 using System;
+using System.Linq;
 
 namespace FirstPractical
 {
@@ -198,34 +199,203 @@ namespace FirstPractical
               */
 
             //Switch Statement
-            int day = 2;
-            switch (day)
+            /* int day = 2;
+             switch (day)
+             {
+                 case 1:
+                     Console.WriteLine("Sunday");
+                     break;
+                 case 2:
+                     Console.WriteLine("Monday");
+                     break;
+                 case 3:
+                     Console.WriteLine("Tuesday");
+                     break;
+                 case 4:
+                     Console.WriteLine("Wednesday");
+                     break;
+                 case 5:
+                     Console.WriteLine("Thursday");
+                     break;
+                 case 6:
+                     Console.WriteLine("Friday");
+                     break;
+                 case 7:
+                     Console.WriteLine("Saturday");
+                     break;
+                 default:
+                     Console.WriteLine("Please enter the correct data");
+                     break;
+             }
+
+             */
+
+            //Loop
+            //while
+            /* int num = 0; 
+
+             while (num < 5)
+             {
+                 Console.WriteLine(num);
+                 num++;
+             }
+
+             //Do/While
+             int num = 0;
+             do
+             {
+                 Console.WriteLine(num);
+                 num++;
+             } while (num < 5);
+              
+
+            //for loop
+            for (int num =0; num<5; num++)
             {
+                Console.WriteLine(num);
+            }
+         
+
+            //foreach loop
+            string[] fruits = { "Banana", "Mango", "Apple", "Grapes", "Pear" };
+            foreach(string num in fruits)
+            {
+                Console.WriteLine(num);
+            }
+
+            
+
+            for(int i=0; i<10; i++)
+            {
+                if (i == 5)
+                    break;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("I'm out");
+             
+
+            for(int i=0; i<10; i++)
+            {
+                if (i == 5)
+                    continue;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("|I'm out");
+            */
+
+            //Arrays
+            /* string[] fruits = { "Banana", "Mango", "Apple", "Grapes", "Pear" };
+
+             //Accessing individual element
+             Console.WriteLine(fruits[0]);
+             Console.WriteLine(fruits[1]);
+             Console.WriteLine(fruits[2]);
+             Console.WriteLine(fruits[3]);
+             Console.WriteLine(fruits[4]);
+
+             //Changing value of specific elements.
+             fruits[2] = "Gauva";
+
+             Console.WriteLine(fruits[2]);
+             Console.WriteLine("\n");
+
+             for(int i=0; i<fruits.Length; i++)
+             {
+                 Console.WriteLine(fruits[i]);
+             }
+
+
+            int[] numbers = { 4, 2, 5, 1, 7, 8, 9, 6, 10 };
+
+            Console.WriteLine("Before Sorting\n");
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\nAfter Sorting\n");
+            Array.Sort(numbers);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+          
+
+            //Linq
+            int[] numbers = { 4, 2, 5, 1, 7, 8, 9, 6, 10 };
+
+            int max = numbers.Max();
+            int min = numbers.Min();
+            int sum = numbers.Sum();
+
+            Console.WriteLine("\nMaximum Value" + max);
+            Console.WriteLine("\nMinimum Value\n" + min);
+            Console.WriteLine("\nSummation of Numbers\n" + sum);
+           
+
+            string fruits = new string[4];
+
+            string fruits2 = new string[4] { "Apple", "Mango", "Banana", "Grapes" };
+
+            string[] fruits3 = new string[] { "Apple", "Mango", "Banana", "Grapes" };
+
+            string[] fruits4 = { "Apple", "Mango", "Banana", "Grapes" };
+            */
+
+            //Menu Choice using the customer input
+            Console.WriteLine("============Welocme to ABC Bank==============");
+            Console.WriteLine("==============MENU==========================");
+            Console.WriteLine("1. Fast Cash");
+            Console.WriteLine("2. Cash Withdraw");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice){
                 case 1:
-                    Console.WriteLine("Sunday");
+                    Console.WriteLine("=====FAST CASH======");
+                    Console.WriteLine("1. 500");
+                    Console.WriteLine("2. 1000");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    switch (choice2)
+                    {
+                        case 1:
+                            Console.WriteLine("You have successfully withdrawn 500. \n Please collect your money.");
+                            break;
+                        case 2:
+                            Console.WriteLine("You have successfully withdrawn 1000. \n Please collect your money.");
+                            break;
+                        default:
+                            Console.WriteLine("Please choose the option in the menu");
+                            break;
+                    }
                     break;
                 case 2:
-                    Console.WriteLine("Monday");
+                    Console.WriteLine("========CASH WITHDRAW=========");
+                    Console.WriteLine("Enter the Cash (in multiple of 500): ");
+                    int choice3 = Convert.ToInt32(Console.ReadLine());
+                    /*if (choice3 % 500 == 0)
+                    {
+                        Console.WriteLine($"You have successfully withdrawn {choice3}. \n Please collect your money.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter the money in the multiple of 500");
+                    }
+                    */
+                  string statement = (choice3 % 500 == 0) ? $"You have successfully withdrawn {choice3}. \n Please collect your money" : "Please enter the money in the multiple of 500";
+                    Console.WriteLine(statement);
                     break;
                 case 3:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 5:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 6:
-                    Console.WriteLine("Friday");
-                    break;
-                case 7:
-                    Console.WriteLine("Saturday");
                     break;
                 default:
-                    Console.WriteLine("Please enter the correct data");
+                    Console.WriteLine("Wrong Input");
                     break;
+
             }
+            Console.WriteLine("Thank you for doing banking with us");
+
+
+
 
             Console.ReadLine();  // It will hold the console until a button is pressed.
         }
